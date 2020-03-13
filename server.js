b5a342a955f623df9061becb8001aa69244776bc
JoshuaHolloway <jhollow6@asu.edu>
2020-03-13 16:32:00 -0500

    node server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 const express = require('express');
+const app = express();
+
+app.use(express.static('static_files'));
+
+const port_num = 8888;
+app.listen(port_num, () => console.log('http://localhost:8888') );
