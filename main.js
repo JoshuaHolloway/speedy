3ea1e0243403b7f5f6249ec57eb37f4a27bdb6ae
JoshuaHolloway <jhollow6@asu.edu>
2020-03-13 16:23:41 -0500

    total protein

--- main.js
+++ main.js
@@ -10,27 +10,35 @@ let matrix = make_matrix(2, 2);
 const foods = {
     grapenuts: {
         cals: 200,
-        fat: {
-            total: 1,
-            saturated: 0,
-            trans: 0,
-            poly: 0.5,
-            mono: 0
-        },
-        cholesterol: 0,
-        sodium: .280,
-        carbs: {
-            total: 47,
-            fiber: {
-                soluble: 1,
-                insoluble: 6
+        macro: {
+            fat: {
+                total: 1,
+                saturated: 0,
+                trans: 0,
+                poly: 0.5,
+                mono: 0
             },
-            sugar: {
-                total: 5,
-                added: 0
-            }
+            carbs: {
+                total: 47,
+                fiber: {
+                    soluble: 1,
+                    insoluble: 6
+                },
+                sugar: {
+                    total: 5,
+                    added: 0
+                }
+            },
+            protein: 6   
         },
-        protien: 6
+        micro: {
+            cholesterol: 0,
+            sodium: .280,
+            D: 0,
+            calcium: 0.02,
+            iron: 0.0162,
+            potassium: 0.260
+        }
     }
 };
 // ----------------------------------------------
@@ -48,12 +56,12 @@ submit.addEventListener('click', () => {
     const food_quantity = matrix[0][0];
 
     const total_cals = foods.grapenuts.cals * food_quantity;
-    const total_protien = foods.grapenuts.protien  * food_quantity;
-    const total_fat = foods.grapenuts.protien  * food_quantity;
+     const total_protein = foods.grapenuts.macro.protein  * food_quantity;
+    // const total_fat = foods.grapenuts.protien  * food_quantity;
 
 
     // Update daily totals
     document.querySelectorAll('.Row .total_cals')[0].innerHTML = total_cals;
-    document.querySelectorAll('.Row .total_fat')[0].innerHTML = total_protien;
+    document.querySelectorAll('.Row .total_protein')[0].innerHTML = total_protein;
 });
 // ----------------------------------------------
