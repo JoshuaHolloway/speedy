3963c853f1f88dcad47f9cba18116c71d140fd85
JoshuaHolloway <jhollow6@asu.edu>
2020-03-23 10:43:02 -0500

    state

--- react/front/src/App.js
+++ react/front/src/App.js
@@ -24,7 +24,6 @@ function App() {
           Learn React
         </a>
 
-        <Chart/>
 
         <br/>
 
--- react/front/src/components/scatter.js
+++ react/front/src/components/scatter.js
@@ -1,43 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Scatter} from 'react-chartjs-2';
 
 const ScatterExample = () => {
 
-  const data = {
-    labels: ['Scatter'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: [
-          { x: 65, y: 75 },
-          { x: 59, y: 49 },
-          { x: 80, y: 90 },
-          { x: 81, y: 29 },
-          { x: 56, y: 36 },
-          { x: 55, y: 25 },
-          { x: 40, y: 18 },
-        ]
-      }
-    ]
+  const [a, b] = useState(
+    {
+      labels: ['Scatter'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [
+            { x: 65, y: 75 },
+            { x: 59, y: 49 },
+            { x: 80, y: 90 },
+            { x: 81, y: 29 },
+            { x: 56, y: 36 },
+            { x: 55, y: 25 },
+            { x: 40, y: 18 },
+          ]
+        }
+      ]
+    }
+  );
+
+  // https://github.com/pomber/covid19
+  const corona_button_click = (event) => {
+    console.log('CORONA button click!');
+
+    const url = 'https://pomber.github.io/covid19/timeseries.json';
+    fetch(url)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => console.log(data.Canada));
+
+      b(
+        {
+          labels: ['Scatter'],
+          datasets: [
+            {
+              label: 'My First dataset',
+              fill: false,
+              backgroundColor: 'rgba(75,192,192,0.4)',
+              pointBorderColor: 'rgba(75,192,192,1)',
+              pointBackgroundColor: '#fff',
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+              pointHoverBorderColor: 'rgba(220,220,220,1)',
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: [
+                { x: 5, y: 75 },
+                { x: 5, y: 49 },
+                { x: 5, y: 90 },
+                { x: 5, y: 29 },
+                { x: 5, y: 36 },
+                { x: 5, y: 25 },
+                { x: 5, y: 18 },
+              ]
+            }
+          ]
+        }
+      );
   };
 
-      return (
-        <div>
-          <h2>Scatter Example</h2>
-          <Scatter data={data} />
-        </div>
-      );
+  return (
+    <div>
+      <button onClick={corona_button_click}>JOSH!</button>
+      <br/>
+      {/* <Scatter data={data} /> */}
+      <Scatter data={a} />
+    </div>
+  );
 };
 
 export default ScatterExample;
